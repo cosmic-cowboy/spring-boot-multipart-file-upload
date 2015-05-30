7bb108a4d61dd19642a8cbf10b4eff1daf90ac01
hiroo takaku <slgerkamp@gmail.com>
2015-05-30 17:11:42 +0900

    プロジェクト作成

---
+++ spring-boot-multipart-file-upload/src/main/java/com/slgerkamp/introductory/spring/boot/multipart/file/upload/Application.java
@@ -0,0 +1,18 @@
+package com.slgerkamp.introductory.spring.boot.multipart.file.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Spring Boot のアプリケーション。
+ *
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(final String[] args) throws Exception {
+		
+		SpringApplication.run(Application.class, args);
+		
+	}
+}
